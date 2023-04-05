9a2c9609a71681acda9a02470978ff8468d26fb8
Denys Lozynskyi <denislozynskyi@gmail.com>
2023-04-05 17:44:13 +0300

    [14] clear app test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,1 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('render The Magic Place text', () => {
-  render(<App />);
-  const text = screen.getByText('The Magic Place');
-  expect(text).toBeInTheDocument();
-});
+export {};
