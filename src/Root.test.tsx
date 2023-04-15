02dc65511b3142055ab82c35e6cf9673edd4cbd3
olha-dovbush <olha.dovbush@jito.dev>
2023-04-15 16:45:11 +0300

    [17] create vitest config

---
+++ src/Root.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { expect } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
+import { Root } from './Root';
+
+test('show links', () => {
+  render(
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
+  );
+  const links = screen.getAllByRole('link');
+  expect(links).toHaveLength(7);
+});
