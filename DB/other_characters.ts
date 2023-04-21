2a1051346c414bd2479705908f07b758bbecc9df
olha-dovbush <olha.dovbush@jito.dev>
2023-04-21 15:36:37 +0300

    create 3 characters

---
+++ DB/other_characters.ts
@@ -0,0 +1,86 @@
+const OTHER_EN = [
+  {
+    name: 'Dudley Dursley',
+    portrait: '',
+    description:
+      'Was the Muggle son of Vernon and Petunia Dursley and cousin of Harry Potter. He was obese and insolent as a result of his parents spoiling him throughout his childhood, although he became muscular in his teens.',
+    interesting_fact:
+      'After the Second Wizarding War, he married a woman and had two children. He also remained in contact with Harry, as did his wife and children.',
+  },
+  {
+    name: 'Petunia Dursley',
+    portrait: '',
+    description:
+      'Was an English Muggle woman, the elder daughter of Mr and Mrs Evans and the older sister of the Muggle-born witch Lily Potter.',
+    interesting_fact:
+      'During her childhood, she became resentful of her sister for being a witch, broke off contact with her sister and refused to have any contact with the wizarding world.',
+  },
+  {
+    name: 'Vernon Dursley',
+    portrait: '',
+    description:
+      'Was an English Muggle, husband of Petunia Evans, father of Dudley Dursley and uncle by marriage of Harry Potter.',
+    interesting_fact:
+      'Dursley is a town in Gloucestershire, England. J. K. Rowling has commented that she visited the place as a child and hated it, which likely affected her choice of surname for the awful family. Rowling chose the name because it sounded "dull and forbidding".',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+];
+
+const OTHER_UA = [
+  {
+    name: 'Дадлі Дурслі',
+    portrait: '',
+    description: 'Син Петунії і Вернона Дурслі, двоюрідний брат Гаррі Поттера, старший за нього на місяць.',
+    interesting_fact:
+      'Після Другої чарівної війни він одружився і мав двох дітей. Він також підтримував зв’язок із Гаррі, як і його дружина та діти.',
+  },
+  {
+    name: 'Петунія Дурслі',
+    portrait: '',
+    description: 'Дружина Вернона Дурслі і мати Дадлі, а також - старша сестра Лілі Поттер, матері Гаррі.',
+    interesting_fact:
+      'У дитинстві вона образилася на свою сестру за те, що вона відьма, розірвала з сестрою контакт і відмовилася від будь-яких контактів зі світом чарівників.',
+  },
+  {
+    name: 'Вернон Дурслі',
+    portrait: '',
+    description: 'Був маглом, чоловіком Петунії Еванс, батьком Дадлі Дурслі та дядьком Гаррі Поттера.',
+    interesting_fact:
+      'Дурслі — місто в графстві Глостершир, Англія. Дж. К. Роулінг прокоментувала, що відвідувала це місце в дитинстві і ненавиділа його, що, ймовірно, вплинуло на її вибір прізвища для цієї жахливої родини. Роулінг вибрала таку назву, тому що вона звучала «нудно й непристойно».',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+];
