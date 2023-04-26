26eeeafb055657eabe928a595a763ea0a2b80f23
olha-dovbush <olha.dovbush@jito.dev>
2023-04-26 09:52:15 +0300

    [DB] add more characters

--- DB/other_characters.ts
+++ DB/other_characters.ts
@@ -24,6 +24,84 @@ const OTHER_EN = [
       'Dursley is a town in Gloucestershire, England. J. K. Rowling has commented that she visited the place as a child and hated it, which likely affected her choice of surname for the awful family. Rowling chose the name because it sounded "dull and forbidding".',
   },
   {
+    name: 'Argus Filch',
+    portrait: '',
+    description: 'Squib and the caretaker of Hogwarts School of Witchcraft and Wizardry since around 1973.',
+    interesting_fact:
+      'Filch usually wandered around the school corridors with his cat Mrs Norris, trying to catch students breaking the school rules and complaining about Peeves the Poltergeist.',
+  },
+  {
+    name: 'Madam Poppy Pomfrey',
+    portrait: '',
+    description: 'A British or Irish witch and the matron at Hogwarts School of Witchcraft and Wizardry.',
+    interesting_fact:
+      'Being the Hogwarts Matron, Madam Pomfrey was a talented Healer who was very well-versed in medical magic. She had a very high understanding of medical charms and could mend broken bones in seconds.',
+  },
+  {
+    name: 'Olympe Maxime',
+    portrait: '',
+    description: 'A French witch, The headmistress of Beauxbatons Academy of Magic.',
+    interesting_fact:
+      'Maxime\'s huge size was due to her half-giant background. Initially, she fiercely denied this, claiming that she merely had "big bones".',
+  },
+  {
+    name: 'Madam Irma Pince',
+    portrait: '',
+    description: 'A witch and librarian at the library of Hogwarts School of Witchcraft and Wizardry.',
+    interesting_fact:
+      "She was highly possessive and protective of the school's books and was deemed highly unpleasant by most of the student body.",
+  },
+  {
+    name: 'Grawp',
+    portrait: '',
+    description:
+      'He was the son of Fridwulfa and an unnamed giant and, therefore, the maternal half-brother of Rubeus Hagrid',
+    interesting_fact:
+      'Was known for his small stature and short height; although he stood at sixteen feet tall, this was considered short by giant standards.',
+  },
+  {
+    name: 'Griphook',
+    portrait: '',
+    description: 'A goblin who worked at Gringotts Wizarding Bank in Diagon Alley.',
+    interesting_fact:
+      "He assisted Harry Potter in his break-in of Gringotts in order to obtain Helga Hufflepuff's Cup.",
+  },
+  {
+    name: 'Mundungus Fletcher',
+    portrait: '',
+    description:
+      'A half-blood wizard who was involved in many illegal activities, having a notoriously disreputable reputation as a petty criminal.',
+    interesting_fact:
+      'However, despite his illicit dealings, he was one of the original members of the Order of the Phoenix.',
+  },
+  {
+    name: 'Nymphadora Tonks',
+    portrait: '',
+    description: 'More commonly known as Tonks, was a British half-blood witch and a Metamorphmagus.',
+    interesting_fact:
+      'In the story, Tonks first appeared in the fifth book. She arrives as part of a group of wizards to transport Harry Potter from Little Whinging to the London headquarters of the Order of the Phoenix.',
+  },
+  {
+    name: 'Xenophilius Lovegood',
+    portrait: '',
+    description:
+      'A wizard who worked as the editor of The Quibbler magazine, the husband of Pandora, and the father of Luna Lovegood.',
+    interesting_fact:
+      'His wife died when their only child was nine years old, and he raised his daughter by himself. He believed in the existence of the Deathly Hallows and supported Harry in The Quibbler when few others would.',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: '',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
     name: '',
     portrait: '',
     description: '',
@@ -66,19 +144,100 @@ const OTHER_UA = [
       'Дурслі — місто в графстві Глостершир, Англія. Дж. К. Роулінг прокоментувала, що відвідувала це місце в дитинстві і ненавиділа його, що, ймовірно, вплинуло на її вибір прізвища для цієї жахливої родини. Роулінг вибрала таку назву, тому що вона звучала «нудно й непристойно».',
   },
   {
-    name: '',
+    name: 'Арґуч Філч',
+    portrait: '',
+    description: 'Сквиб та доглядач Хогвартсу, школи магії і чаклунства, з приблизно 1973 року.',
+    interesting_fact:
+      'Філч зазвичай блукав по коридорах школи зі своєю кішкою місіс Норріс, намагаючись впіймати учнів за порушення шкільних правил та скаржився на Півза - полтергейста.',
+  },
+  {
+    name: 'Мадам Поппі Пофрі',
+    portrait: '',
+    description: 'Британська або ірландська відьма та медсестра у Хогвартській школі магії і чаклунства.',
+    interesting_fact:
+      'Мадам Помфрей була талановитою Відновлювачкою, яка добре володіла медичною магією. Вона мала високе розуміння медичних чарів і могла зцілювати переломи кісток за кілька секунд.',
+  },
+  {
+    name: 'Олімпія Максім',
+    portrait: '',
+    description: 'Французька чарівниця, директор Академії магії Бобатон.',
+    interesting_fact:
+      'Мадам Максім приховує факт, що серед її предків були велетні, стверджуючи, що «просто широка в кістки». І, загалом-то, вона чинить правильно. У магічній спільноті велетні користуються поганою славою.',
+  },
+  {
+    name: 'Ірма Пінс',
+    portrait: '',
+    description: 'Завідувачка бібліотекою в школі чаклунства Гоґвортс.',
+    interesting_fact:
+      'Сувора жінка «бальзаківського віку», «схожа на голодного стервятника». Дуже трепетно ставиться до книг, як тільки може, оберігає їх від студентів.',
+  },
+  {
+    name: 'Ґроп',
+    portrait: '',
+    description: 'Велетень, єдиноутробний брат Гегрід а (велетень Фрідвульфа народила їх від різних батьків). Сирота.',
+    interesting_fact:
+      'Був відомий своїм маленьким зростом; хоча він мав шістнадцять футів у висоту, це вважалося низьким за вимірами велетнів.',
+  },
+  {
+    name: 'Ґрипхук',
+    portrait: '',
+    description: 'Ґоблін, який працював в банку Ґрінґотс.',
+    interesting_fact:
+      'Він допомагав Гаррі Поттеру під час його вторгнення в Грінготтс з метою отримання кубка Гелги Гаффлпафф.',
+  },
+  {
+    name: 'Манданґус Флетчер',
+    portrait: '',
+    description: 'Член Ордену Фенікса. Знає всіх шахраїв, оскільки «сам з їх числа».',
+    interesting_fact:
+      'Найбільш безвідповідальний член Ордена, хоча служить вірно. Можливо, причина в тому, що свого часу Албус Дамблдор — глава Ордена — «врятував його від великої неприємності». Так, в усякому разі, каже Сіріус Блек.',
+  },
+  {
+    name: 'Німфадора Тонкс',
+    portrait: '',
+    description: 'Метаморфомаг, аврор, член Ордену Фенікса. Дружина Ремуса Люпина, мати Тедді.',
+    interesting_fact:
+      "В оповіданні Тонкс вперше з'явилася в п'ятій книзі. Вона прибуває в складі групи чарівників, щоб переправити Гаррі Поттера з Літтл Вінґіну в Лондонську штаб-квартиру Ордену Фенікса на пл. Гримо, 12.",
+  },
+  {
+    name: 'Ксенофілій Лавґуд',
+    portrait: '',
+    description: 'Батько Луни Лавгуд. Вдівець. Видавець журналу «Базікало».',
+    interesting_fact:
+      'Його дружина померла, коли їхній дитині було дев\'яти років, і він самотужки виховував дочку. Він вірив в існування Смертельних Реліквій і підтримував Гаррі в газеті "Базікалло", коли мало хто інший це робив.',
+  },
+  {
+    name: 'Белатриса Лестранж',
     portrait: '',
     description: '',
     interesting_fact: '',
   },
   {
-    name: '',
+    name: 'Сіріус Блек',
     portrait: '',
     description: '',
     interesting_fact: '',
   },
   {
-    name: '',
+    name: 'Артур Візлі',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: 'Луціус Мелфой',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: 'Віктор Крум',
+    portrait: '',
+    description: '',
+    interesting_fact: '',
+  },
+  {
+    name: 'Пітер Петіґру',
     portrait: '',
     description: '',
     interesting_fact: '',
