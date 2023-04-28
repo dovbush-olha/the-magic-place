cdc23b9daf4ba3b224bcdda2950f116bcc299758
olha-dovbush <olha.dovbush@jito.dev>
2023-04-28 12:45:23 +0300

    [DB] create Houses DB

---
+++ client/DB/Houses.ts
@@ -0,0 +1,268 @@
+type House = {
+  image: string;
+  title: string;
+};
+
+export const gryffindorHouse: House[] = [
+  {
+    image: '',
+    title:
+      'Hogwarts students who are sorted into Gryffindor House are known to be brave, courageous, daring, and determined. Its crest is depicts a lion’s head, and its colors are gold and scarlet.',
+  },
+  {
+    image: '',
+    title:
+      'Gryffindor tower looks over the Quidditch Pitch, Hogwarts Lake, and the Forbidden Forest. It features a portrait of Godric Gryffindor hanging over the fireplace, which is surrounded by cozy chairs. Other portraits of former Heads of Gryffindor as well as mythical creatures adorn the walls.',
+  },
+  {
+    image: '',
+    title:
+      'McGonagall is the last known Head of House for Gryffindor. She dropped the position when she took over as Headmaster of Hogwarts in 1998 after the Second Wizarding War, and there is no record of her successor.',
+  },
+  {
+    image: '',
+    title:
+      'Members of Gryffindor seem to have an aptitude for Transfiguration. Albus Dumbledore and Minerva McGonagall were both former professors (and Gryffindor students) in the subject, Hermione Granger is said to be very talented at transfiguration, and there are four known Animagi from Gryffindor: McGonagall (cat), James Potter (stag), Sirius Black (dog) and Peter Pettigrew (rat).',
+  },
+  {
+    image: '',
+    title:
+      'Long before the Sorting Hat was, well, the Sorting Hat, it belonged to Godric Gryffindor. The Hat’s song even talks about how Gryffindor was the one who had the idea of turning his normal hat into something much more, although all the founders had a hand in creating the Sorting Hat as we know it today.',
+  },
+  {
+    image: '',
+    title:
+      'J.K. Rowling admitted during an interview that when she took the sorting quiz on Pottermore, she was a Gryffindor! She was a bit surprised, and said that she actually had soft spot for Hufflepuff.',
+  },
+];
+
+const gryffindorHouseUKR: House[] = [
+  {
+    image: '',
+    title:
+      'Учні, які потрапляють до Гріффіндору, славляться своєю хоробрістю, відвагою, сміливістю та наполегливістю. На їх гербі зображено голову лева, а кольори - золотий і багряний.',
+  },
+  {
+    image: '',
+    title:
+      'Вежа Гріффіндору височіє над полями квіддичу, озером Хогвартсу та Забороненим лісом. Вона прикрашена портретом Годрика Гріффіндора, який розташований над каміном, оточеним затишними кріслами. На стінах можна побачити портрети колишніх очільників Гріффіндору, а також зображення міфічних істот.',
+  },
+  {
+    image: '',
+    title:
+      'МакҐонагол є останньою відомою очільницею Гріффіндору. Вона покинула посаду, коли перейняла посаду директора Хогвартсу в 1998 році після Другої світової війни чаклунів, і немає жодних записів про її наступника.',
+  },
+  {
+    image: '',
+    title:
+      'Члени Гріффіндору володіють винятковою здібністю до трансфігурації. Альбус Дамблдор і Мінерва Макґонагол були колишніми викладачами (і самі студенти Гріффіндору) у цьому предметі, Герміона Ґрейнджер відома своєю високою майстерністю в трансфігурації, а також чотири відомих анімаги з Гріффіндору: Макґонагол (кіт), Джеймс Поттер (олень), Сіріус Блек (собака) та Пітер Петтігрю (щур).',
+  },
+  {
+    image: '',
+    title:
+      'Задовго до того, як Сортувальний Капелюх став тим, чим ми його знаємо, він належав Годрику Гріффіндору. У пісні Капелюха згадується, як саме Гріффіндору прийшла ідея перетворити свій звичайний капелюх на щось більш корисне, хоча всі засновники дали свій внесок у створення Сортувального Капелюха, яким ми його знаємо сьогодні.',
+  },
+  {
+    image: '',
+    title:
+      "У під час інтерв'ю Дж.К. Роулінг зізналася, що коли проходила відбір на Поттерморі, вона виявилася Гріффіндорцем! Вона була трохи здивована і сказала, що насправді має слабкість до Хаффлпафу.",
+  },
+];
+
+export const slytherinHouse: House[] = [
+  {
+    image: '',
+    title:
+      'Salazar Slytherin was obsessed with pure-bloods, and even the Sorting Hat admits that wizard-born blood is a factor in its Sorting of a single-character into the mean, green House. Nonetheless, technically anyone displaying the qualities Slytherin broadly valued—cunning, resourcefulness, and ambition—may be sorted into Slytherin. It is, however, very rare for Muggle-borns to end up as Slytherins.',
+  },
+  {
+    image: '',
+    title:
+      'While Gryffindor House boasts a cozy, tapestry-filled common room, Slytherin students call quite a different common room and sleeping quarters home. Their dormitories are housed underneath the Black Lake in Slytherin Dungeon, and to access Slytherin central you must speak the password in front of a damp stone wall. The only known password, “pureblood,” was used in 1992.',
+  },
+  {
+    image: '',
+    title:
+      'Who wants to wear scarlet and gold anyway? Slytherin’s house colors of green and silver are classy, timeless, and oh-so-chic. Emerald green dress robes? Sign me up for the next Dark wizard war!',
+  },
+  {
+    image: '',
+    title:
+      'Despite them seeming only to look after their own necks, don’t count Slytherins out as disloyal. They tend to pick their friends very carefully and will defend their own to the death. Remember how badly Draco wanted Harry in his corner?',
+  },
+  {
+    image: '',
+    title:
+      'Besides Slytherin himself, there have been two notable Heads of House: Horace Slughorn, the professor who first told Tom Riddle about the Horcruxes, once served as top snake, and everyone’s favorite tragic hero, Severus Snape, was also Head of Slytherin for a time.',
+  },
+  {
+    image: '',
+    title:
+      'Each Hogwarts House is linked to a different element: despite their green emblem, Slytherin is actually connected to water. Snakes have long been associated with water, and have a fluidity and flexibility that recalls water’s amorphous form.',
+  },
+  {
+    image: '',
+    title:
+      'It’s worth repeating: not all Slytherins are bad or destined for evil. In fact, the Harry Potter’s own son, Albus Severus, was sorted into Slytherin upon his arrival at Hogwarts. This is hinted at in The Deathly Hallows, but fully revealed in The Cursed Child. He’ll do the House proud—and maybe set them straight a little.',
+  },
+];
+
+export const slytherinHouseUKR: House[] = [
+  {
+    image: '',
+    title:
+      'Салазар Слизерин був одержимий чистокровними, і навіть Сортувальний Капелюх зізнається, що кров чаклуна є фактором у відборі до зловісного зеленого гуртожитку. Однак, технічно будь-хто, хто виявляє риси, які Слизерин цінував - хитрість, винахідливість та амбіційність - може бути відібраний до цього гуртожитку. Однак, дуже рідко Маглородні знаходяться в Слизерині.',
+  },
+  {
+    image: '',
+    title:
+      'Хоча у Гріффіндорі є затишна, заповнена гобеленом спільна кімната, учні Слизерину мають зовсім іншу вітальню та спальні. Їхні спальні розміщені під чорним озером в підвалах Слизерину, а щоб потрапити до центральної частини Слизерину, потрібно вимовити пароль перед вологою кам\'яною стіною. Єдиний відомий пароль, "чистокровність", був використаний у 1992 році.',
+  },
+  {
+    image: '',
+    title:
+      'Хто ж хоче носити червоне та золоте? Кольори Слизерину - зелений та срібний - це класика, вічність та надзвичайна елегантність. І зелені облягаючі сукні для вечірок? Записуйте мене на наступну війну темних чаклунів!',
+  },
+  {
+    image: '',
+    title:
+      "Незважаючи на те, що Слизеринці здається більше піклуються тільки про себе, не вважайте їх невірними. Вони дуже обережно вибирають своїх друзів і будуть захищати своїх товаришів до останнього подиху. Пам'ятаєте, як сильно Драко хотів мати Гаррі на своєму боці?",
+  },
+  {
+    image: '',
+    title:
+      'Крім самого Слизерина, були два помітні Голови Гуртожитку: Горас Слаггорн, професор, який перший розповів Тому Реддлу про філософський камінь та Горокракси, а також улюблений усіма трагічний герой, Северус Снейп, протягом певного часу очолював Слизерин.',
+  },
+  {
+    image: '',
+    title:
+      "Кожен гуртожиток Хогвартсу пов'язаний з різним елементом: незважаючи на їх зелений знак, Слизерин насправді пов'язаний з водою. Змії давно асоціюються з нею і мають плинність та гнучкість, які нагадують аморфну форму води.",
+  },
+  {
+    image: '',
+    title:
+      'Варто повторити, що не всі Слизеринці є поганими або приреченими на зло. Насправді, син Гаррі Поттера, Альбус Северус, був відсортований до Слизерину при своєму приїзді до Хогвартсу. Про це є натяк в "Смертельних Реліквіях", але повністю розкривається в "Проклятому Диті". Він принесе гідність своєму будинку - і може трохи покращить його репутацію.',
+  },
+];
+
+export const hufflepuffHouse: House[] = [
+  {
+    image: '',
+    title:
+      'Hufflepuff house is commonly known for their kindness and loyalty. Even though they are able to excel in almost all of the wizarding areas (yes, even Defense Against the Dark Arts) their incredible ability to be good hearted people has kept them from going over to the dark side. Hufflepuff is the house that has the fewest dark wizards and witches in wizarding history.',
+  },
+  {
+    image: '',
+    title:
+      'The dormitories in Hufflepuff are the best set up. It is large and uses natural sunlight. It is covered in earthly toned colors creating a cozy and relaxing atmosphere. Who doesn’t want to chill in something like that? Not to mention Hufflepuff is closest to the kitchens. Midnight munchies here we come!',
+  },
+  {
+    image: '',
+    title:
+      'Surely everyone has experienced a haunting and sometimes those experiences suck! If you’re going to be haunted, let’s hope it is not by ghosts like headless Nick who is whinny, or Bloody Baron from Slytherin. Clearly, the Fat Friar aka Double F from Hufflepuff is the ghost you want. Double F is cheerful and a bit mysterious and doesn’t care all that much about school. Thinking of changing houses yet?',
+  },
+  {
+    image: '',
+    title:
+      'Hufflepuffs have very easy going personalities which makes it easy for them to be all rounders when it comes to their abilities. They are far better when it comes to nature, they seem to have a better understanding of herbology and care of magical creatures, but they do well in other subjects like potions and transfiguration as well.',
+  },
+  {
+    image: '',
+    title:
+      'Hufflepuff’s put their friendships over anything else. They like to keep things simple and concentrate in making lifelong friendships. If you have a hufflepuff as one of your close friends, you can consider yourself lucky.',
+  },
+];
+
+export const hufflepuffHouseUKR: House[] = [
+  {
+    image: '',
+    title:
+      'Учні, які потрапляють до хати Хафелпаф, відомі своєю добротою та вірністю. Незважаючи на те, що вони можуть відзначитись майже в усіх галузях магії (так, навіть у захисті від темної магії), їх неймовірна здатність бути добрими людьми захистила їх від перетворення на темну сторону. Хафелпаф - гуртожиток, який має найменшу кількість темних чаклунів та чаклунок у історії світу чаклунства.',
+  },
+  {
+    image: '',
+    title:
+      'Спальні кімнати Хафелпафу влаштовані найкраще. Вони великі та заповнені природнім світлом. Їхні стіни прикрашені земляними тонами, створюючи затишну та розслаблюючу атмосферу. Хто не хотів би там помилуватись? До того ж, Хафелпаф найближче розташований до кухні. Північні перекуси - це вже явний успіх!',
+  },
+  {
+    image: '',
+    title:
+      'Напевно, кожен хоча б раз у своєму житті мав досвід з привидами, і ці досвіди бувають не дуже приємними. Якщо вам все ж таки доведеться бути поміченим якимось привидом, сподіваємося, що це не будуть такі привиди, як Майже-Безголовий Нік, який завжди скаржиться, чи Кровавий Барон з Слизерину. Очевидно, що найкращим вибором буде привид Товстий Монах, або Подвійний Еф, з Хафелпафу. Він завжди веселий та трохи таємничий і не особливо цікавиться навчанням. Можливо, ви вже думаєте про перехід до Хафелпафу?',
+  },
+  {
+    image: '',
+    title:
+      'Представники Хафелпафу мають дуже спокійні особистості, що дозволяє їм бути універсалами у своїх здібностях. Вони виявляються кращими, коли мова йде про природу, вони мають краще розуміння гербалогії і догляду за магічними створіннями, але також добре справляються із іншими предметами, такими як зіллярство та трансфігурація.',
+  },
+  {
+    image: '',
+    title:
+      'Гафелпавці кладуть дружбу вище за все і люблять прості речі. Вони зосереджуються на тому, щоб створити довговічні дружні стосунки. Якщо у вас є гафелпавець у списку своїх близьких друзів, то вважайте себе щасливими.',
+  },
+];
+
+export const ravenclawHouse: House[] = [
+  {
+    image: '',
+    title:
+      'The original Ravenclaw colors are actually blue and bronze. These colors are taken from the sky (blue) and the feathers of the eagle that is the house symbol (bronze).',
+  },
+  {
+    image: '',
+    title:
+      "The Ravenclaw common room is known for having the best views of the Hogwarts grounds, and it's found at the top of a high spiral staircase. The main common room features several high, arched windows to take advantage of the views, and the dorms are in turrets, with four poster beds hung with sky blue silk. As well as being a house for the wisest of the students, Ravenclaw is definitely not one for students who are afraid of heights!",
+  },
+  {
+    image: '',
+    title:
+      "Ravenclaw’s association with air is also connected to its other symbols. Like the eagle and the sky blue house colors, air is related to the Ravenclaws' lofty heights of learning. Air is also associated with clear sight (as is the eagle itself, hence the phrase ‘eagle eyed’), elevation (namely, the elevation of the mind through learning) and a longstanding connection to the spirit and the mind. ",
+  },
+  {
+    image: '',
+    title:
+      'In order to enter the room, students must answer a riddle posed by the eagle, who will only open the door to correct Answers. This can often be time-consuming, and Ravenclaws quickly learn to triple check that they have everything before leaving the tower – as getting back in can take a while!Some of the riddles are quite difficult, and there can even be a crowd of students in front of the eagle all attempting to work it out together (which helps the Ravenclaws socialize, at least).',
+  },
+  {
+    image: '',
+    title:
+      'The famous wandmaker Garrick Ollivander is a Ravenclaw, as is Ignatia Wildsmith, who invented floo powder. The invention of floo powder is particularly notable, as this transportation powder is much faster than traveling by broom, and doesn’t require a license, as apparating does. The formula for Floo powder is also a closely guarded secret, and although many others have attempted to make their own, Ignatia Wildsmith’s secret remains just that. In fact, several witches and wizards have ended up in the hospital trying to replicate it over the years.',
+  },
+  {
+    image: '',
+    title:
+      'Ravenclaw prizes curiosity, and encourages students to explore arcane and unusual forms of magic. As a result, many of the famous Ravenclaws are quite odd. Ex-Minister Lorcan McLaird communicated with puffs of smoke, and Uric the Oddball (the punchline of many magical jokes) was known for wearing a jellyfish as a hat! The most famous unique Ravenclaw of the series, however, has to be Luna Lovegood, with her devotion to the Quibbler and her one of a kind fashion sense.',
+  },
+];
+
+export const ravenclawHouseUKR: House[] = [
+  {
+    image: '',
+    title:
+      'Оригінальні кольори Рейвенклову сині та бронзові. Ці кольори взяті з неба (синій) та пера орла, який є символом дому (бронзовий).',
+  },
+  {
+    image: '',
+    title:
+      "Відомо, що вітальня Рейвенклову має найкращий вид на територію Хогвартсу, і знаходиться вона у верхніх поверхах, до яких можна дістатися лише піднявшись по крутій сходовій клітці. Основна кімната має кілька високих, кам'яних вікон, а спальні розташовані в вежах, з ліжками, прибраними блакитним шовком. Рейвенклов — це не тільки гуртожиток для найрозумніших студентів, а й місце для тих, хто не боїться висоти!",
+  },
+  {
+    image: '',
+    title:
+      "Асоціація Рейвенклову з повітрям пов'язана також з її іншими символами. Як орел та блакитно-небесні кольори палацу, повітря пов'язане з висотами навчання Рейвенкловців. Повітря також пов'язане з чітким баченням (як і сам орел, звідки і фраза \"орлиний погляд\"), висотою (а саме, висотою розуму через навчання) та довготривалим зв'язком із духом та розумом.",
+  },
+  {
+    image: '',
+    title:
+      'Щоб потрапити в кімнату, студентам потрібно відповісти на загадку, яку ставить орел, що відкриє двері лише для правильних відповідей. Це часто займає багато часу, і Рейвенкловці дуже швидко навчаються тричі перевіряти, чи вони все мають перед виходом з вежі - так як потрапити назад може зайняти довгий час! Деякі загадки досить складні, і навіть може бути група студентів перед орлом, які разом намагаються зрозуміти її (що принаймні допомагає студентам знайомитися між собою).',
+  },
+  {
+    image: '',
+    title:
+      'Знаменитий виробник паличок Оллівандер та Іґнатія Вільдсміт, яка винайшла порошок Флу, належать до Рейвенклов. Винахід порошку Флу особливо відомий, оскільки цей засіб транспортування є набагато швидшим, ніж подорож на мітлі, і не потребує ліцензії. Формула порошку Флу також є засекреченою, і хоча багато інших спробували зробити свій власний порошок, секрет Іґнатії Вільдсміт залишається таємницею. Навіть декілька відьом і чаклунів потрапили до лікарні, намагаючись скопіювати її рецепт протягом років.',
+  },
+  {
+    image: '',
+    title:
+      'Рейвенклов винагороджує допитливість і спонукає учнів досліджувати та вивчати таємничі та неординарні форми магії. Як наслідок, багато хто з відомих Рейвенкловців досить дивні. Екс-міністр Лоркан Маклеєрд спілкувався за допомогою хмаринок диму, а Урік Незвичайний (герой безлічі магічних жартів) був відомий тим, що носив на голові медузу як капелюх! Однак, найбільш відомою та неординарною є Луна Лавґуд, яка є вірною журналу "Базікало" та має неповторний смак до моди.',
+  },
+];
