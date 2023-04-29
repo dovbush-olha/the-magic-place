0c063930c4b0648f489614bb19df2574272a168c
olha-dovbush <olha.dovbush@jito.dev>
2023-04-29 17:30:17 +0300

    [DB] add quotes

---
+++ client/DB/Quotes.ts
@@ -0,0 +1,186 @@
+type Quote = {
+  text: string;
+  author: string;
+};
+
+const quotesEN: Quote[] = [
+  {
+    text: "\"I hope you're pleased with yourselves. We could all have been killed — or worse, expelled. Now if you don't mind, I'm going to bed.\"",
+    author: 'Hermione Granger',
+  },
+  {
+    text: '"It does not do well to dwell on dreams and forget to live."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"To the well-organized mind, death is but the next great adventure."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"You\'re a little scary sometimes, you know that? Brilliant... but scary."',
+    author: 'Ron Weasley',
+  },
+  {
+    text: '"It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"The truth. It is a beautiful and terrible thing, and should therefore be treated with great caution."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"Fear of a name only increases fear of the thing itself."',
+    author: 'Hermione Granger',
+  },
+  {
+    text: '"It is our choices, Harry, that show what we truly are, far more than our abilities."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"Dobby is free."',
+    author: 'Dobby',
+  },
+  {
+    text: '"Training for the ballet, Potter?"',
+    author: 'Draco Malfoy',
+  },
+  {
+    text: '"I’ll be in my bedroom, making no noise and pretending I’m not there."',
+    author: 'Harry Potter',
+  },
+  {
+    text: '"Hearing voices no one else can hear isn’t a good sign, even in the wizarding world."',
+    author: 'Ron Weasley',
+  },
+  {
+    text: '"Fame is a fickle friend, Harry. Celebrity is as celebrity does. Remember that."',
+    author: 'Gilderoy Lockhart',
+  },
+  {
+    text: '"Are you insane? Of course I want to leave the Dursleys! Have you got a house? When can I move in?"',
+    author: 'Harry Potter',
+  },
+  {
+    text: '"You think the dead we loved truly ever leave us? You think that we don’t recall them more clearly in times of great trouble?"',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals."',
+    author: 'Sirius Black',
+  },
+  {
+    text: '"It matters not what someone is born, but what they grow to be."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"Curiosity is not a sin…. But we should exercise caution with our curiosity… yes, indeed."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"We are only as strong as we are united, as weak as we are divided."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"Just because it’s taken you three years to notice, Ron, doesn’t mean no one else has spotted I’m a girl!"',
+    author: 'Hermione Granger',
+  },
+  {
+    text: '"Differences of habit and language are nothing at all if our aims are identical and our hearts are open."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"I mean, it\'s sort of exciting, isn\'t it, breaking the rules?"',
+    author: 'Hermione Granger',
+  },
+  {
+    text: '"We’ve all got both light and dark inside us. What matters is the part we choose to act on. That’s who we really are."',
+    author: 'Sirius Black',
+  },
+  {
+    text: '"Youth can not know how age thinks and feels. But old men are guilty if they forget what it was to be young."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"The thing about growing up with Fred and George is that you sort of start thinking anything\'s possible if you\'ve got enough nerve."',
+    author: 'Ginny Weasley',
+  },
+  {
+    text: '"Dumbledore says people find it far easier to forgive others for being wrong than being right."',
+    author: 'Hermione Granger',
+  },
+  {
+    text: '"Age is foolish and forgetful when it underestimates youth."',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '"We’re all human, aren’t we? Every human life is worth the same, and worth saving."',
+    author: 'Kingsley Shacklebolt',
+  },
+  {
+    text: '"Of course it is happening inside your head, Harry, but why on earth should that mean that it is not real?"',
+    author: 'Albus Dumbledore',
+  },
+  {
+    text: '“Do not pity the dead, Harry. Pity the living, and, above all, those who live without love.”',
+    author: 'Albus Dumbledore',
+  },
+];
+
+const quotesUA: Quote[] = [
+  {
+    text: '"Треба бути дуже хоробрим, щоб чинити опір ворогам, але потрібна не менша хоробрість, щоб чинити опір друзям."',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Поттере, у тебе вигляд цілком здоровий. Отож вибач, що я не звільняю тебе від домашніх завдань. Але запевняю: у разі твоєї смерті виконувати їх необов’язково."',
+    author: 'Мінерва Макґонеґел',
+  },
+  {
+    text: '"Не жалій мертвих, Гаррі. Жалій живих, а понад усе тих, що живуть без любові."',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Авжеж, Гаррі, це відбувається у твоїй голові, але чого ж воно має бути несправжнім?"',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Ми сильні рівно на стільки, на скільки об’єднані, і на стільки ж слабкі — на скільки розділені."',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: 'Щастя існує і в найтемніші часи, якщо не забути ввімкнути світло.',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Рано чи пізно ми всі стаємо перед вибором між тим, що правильно, і тим, що легко."',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Не варто поринати у мрії, забуваючи про життя, пам’ятай про це."',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Байдужість і зневага часто приносять набагато більше шкоди, ніж відкрита неприязнь"',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Не існує нічого неможливого, якщо маєш крихту зухвальства."',
+    author: 'Джіні Візлі',
+  },
+  {
+    text: '"Набагато легше пробачити людей за те, що вони не праві, ніж за правду."',
+    author: 'Албус Дамблдор',
+  },
+  {
+    text: '"Не забувайте про те, що всі великі чарівники в історії свого часу були такими ж, як ми, – школярами. Якщо у них вийшло, то вийде і у нас."',
+    author: 'Гаррі Поттер',
+  },
+  {
+    text: '"Виходить, більше всього на світі ти боїшся страху. Це похвально!"',
+    author: 'Ремус Люпин',
+  },
+  {
+    text: '"У всьому і у всіх є дві сторони: чорна і біла. Важливо лише те, яку сторону вибрав ти. Це визначає все."',
+    author: 'Сіріус Блек',
+  },
+];
