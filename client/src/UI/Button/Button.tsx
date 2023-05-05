cb08ce17ceffbb11313de6d78d91157d027c14fa
olha-dovbush <olha.dovbush@jito.dev>
2023-05-05 16:40:36 +0300

    [44] add test to Button

--- client/src/UI/Button/Button.tsx
+++ client/src/UI/Button/Button.tsx
@@ -5,23 +5,23 @@ import * as Styled from './Button.style';
 interface ButtonProps {
   children: ReactNode;
   type?: 'button' | 'submit';
-  onClick: (event: MouseEventHandler<HTMLButtonElement>) => void;
-  variant: 'primary' | 'secondary';
-  icon: ReactSVGElement;
-  disabled?: boolean;
+  onClick?: (event: MouseEventHandler<HTMLButtonElement>) => void;
+  variant?: 'primary' | 'secondary';
+  icon?: ReactSVGElement;
+  isDisabled?: boolean;
   style?: React.CSSProperties;
 }
 
 export function Button(props: ButtonProps) {
-  const { children, type, onClick, variant = 'primary', icon = <Icon />, disabled = false, style = {} } = props;
+  const { children, type, onClick, variant = 'primary', icon = <Icon />, isDisabled = false, style = {} } = props;
 
   return (
     <Styled.Button
-      type={type}
+      type={type || 'button'}
       onClick={(e) => (onClick ? onClick(e) : undefined)}
       variant={variant}
       icon={icon}
-      disabled={disabled}
+      disabled={isDisabled}
       style={style}
     >
       {icon}
