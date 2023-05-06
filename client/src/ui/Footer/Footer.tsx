47493910df858c1204e63dad1422af5733e68030
Denys Lozynskyi <denislozynskyi@gmail.com>
2023-05-06 12:10:26 +0300

    [43] add ui/

---
+++ client/src/ui/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { Container } from '../Layout';
+
+export function Footer() {
+  return (
+    <Container>
+      <footer>
+        <p>This is footer</p>
+      </footer>
+    </Container>
+  );
+}
---
+++ client/src/ui/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { Container } from '../Layout';
+import * as Styled from './Header.style';
+
+export function Header() {
+  return (
+    <Container>
+      <Styled.Wrapper>
+        <Styled.NavBar>
+          <Styled.NavbarLink to="/">Home</Styled.NavbarLink>
+          <Styled.NavbarLink to="/houses">Logo</Styled.NavbarLink>
+
+          <Styled.RightNav>
+            <Styled.NavbarLink to="/profile">Profile</Styled.NavbarLink>
+            <Styled.NavbarLink to="/settings">Settings</Styled.NavbarLink>
+          </Styled.RightNav>
+        </Styled.NavBar>
+      </Styled.Wrapper>
+    </Container>
+  );
+}
