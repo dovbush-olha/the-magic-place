11c4a6692d80f537b8c680fede93144504c9a736
Denys Lozynskyi <denislozynskyi@gmail.com>
2023-05-16 18:21:03 +0300

    [51] created House model

--- server/src/models/House.js
+++ server/src/models/House.js
@@ -1,7 +1,37 @@
 const mongoose = require('mongoose');
 
-const houseSchema = mongoose.Schema();
+const houseSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    // required: true,
+  },
+});
+
+const houseTitleSchema = mongoose.Schema({
+  houseId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'houses',
+  },
+  titleEn: {
+    type: String,
+    required: true,
+  },
+  titleUk: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+})
 
 const House = mongoose.model('houses', houseSchema);
+const HouseTitle = mongoose.model('houseTitles', houseTitleSchema);
 
-module.exports = House;
+module.exports = {
+  House,
+  HouseTitle,
+};
