dd6eae72172f591d8e523375381949a1669e4a83
olha-dovbush <olha.dovbush@jito.dev>
2023-05-18 21:00:13 +0300

    [48] add everything to background

--- client/src/pages/HomePage/HomePage.style.ts
+++ client/src/pages/HomePage/HomePage.style.ts
@@ -6,9 +6,8 @@ import LeftImage from 'assets/images/home-page/left-image.png';
 export const Wrapper = styled.div`
   background-image: url(${BGImage});
   background-repeat: no-repeat;
-  background-position: center top;
-  background-size: contain;
-  min-height: 100vh;
+  background-position: center top -85px;
+  background-size: 110%;
 `;
 
 export const Background = styled.div`
@@ -43,10 +42,54 @@ export const Background = styled.div`
 `;
 
 export const MainBlock = styled.div`
-  border: 2px solid;
-  border-image-source: linear-gradient(180deg, rgba(255, 246, 229, 0) 43.75%, #fff6e5 100%);
+  min-height: 100%;
+  margin-bottom: 15%;
+  display: flex;
+  flex-direction: column;
 `;
 
-export const MainTop = styled.div``;
+export const MainTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  justify-self: center;
+  align-items: center;
+  padding-top: 60px;
+`;
 
-export const MainBottom = styled.div``;
+export const MainBottom = styled.div`
+  margin: 0 200px;
+  padding: 48px;
+  padding-top: 45%;
+  border: 2px solid;
+  border-image-source: linear-gradient(180deg, rgba(255, 246, 229, 0) 43.75%, #fff6e5 100%);
+  border-image-slice: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+
+  &::before,
+  &::after {
+    position: absolute;
+    content: '';
+    border: 2px solid;
+    border-image-source: linear-gradient(225deg, #fff6e5 0.3%, rgba(255, 246, 229, 0) 39.37%);
+    border-image-slice: 1;
+    bottom: -80px;
+    width: 114px;
+    height: 114px;
+  }
+
+  &::before {
+    left: -80px;
+  }
+
+  &::after {
+    right: -80px;
+    transform: matrix(-1, 0, 0, 1, 0, 0);
+  }
+`;
+
+export const Welcome = styled.h1``;
+
+export const WorldText = styled.h2``;
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Container } from 'ui';
+import { Container, Button } from 'ui';
 
 import * as Styled from './HomePage.style';
 
@@ -8,9 +8,27 @@ export function HomePage() {
       <Container>
         <Styled.Wrapper>
           <Styled.MainBlock>
-            <Styled.MainTop>кнопки</Styled.MainTop>
-            <Styled.MainBottom>The World That is always with you</Styled.MainBottom>
+            <Styled.MainTop>
+              <Button>Research</Button>
+              <Styled.Welcome>Welcome to Hogwarts</Styled.Welcome>
+              <Button>Houses</Button>
+            </Styled.MainTop>
+
+            <Styled.MainBottom>
+              <Styled.WorldText> The world that is always with you...</Styled.WorldText>
+            </Styled.MainBottom>
           </Styled.MainBlock>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+            dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+            sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+            ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+            nulla pariatur?
+          </p>
         </Styled.Wrapper>
       </Container>
     </Styled.Background>
