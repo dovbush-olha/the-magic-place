4a983cb291821b9a6e4c258471ac3d3b792fa8bb
Denys Lozynskyi <denislozynskyi@gmail.com>
2023-05-18 11:38:07 +0300

    [55] add Student schema

--- server/src/models/Students.js
+++ server/src/models/Students.js
@@ -1,6 +1,51 @@
 const mongoose = require('mongoose');
 
-const studentSchema = mongoose.Schema();
+const studentSchema = mongoose.Schema({
+  UK: {
+    name: {
+      type: String,
+      required: true,
+    }  ,
+    portrait: {
+      type: String,
+      // require: true,
+    },
+    house: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    interestingFacts: {
+      type: [String],
+      required: true,
+    },
+  },
+  EN: {
+    name: {
+      type: String,
+      required: true,
+    },
+    portrait: {
+      type: String,
+      // require: true,
+    },
+    house: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    interestingFacts: {
+      type: [String],
+      required: true,
+    },
+  }
+});
 
 const Student = mongoose.model('students', studentSchema);
 
