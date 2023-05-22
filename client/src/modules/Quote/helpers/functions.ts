e6a69e23dcd325239b34cb061532385f74e6c3d2
olha-dovbush <olha.dovbush@jito.dev>
2023-05-22 17:37:15 +0300

    [65] add types to function

--- client/src/modules/Quote/helpers/functions.ts
+++ client/src/modules/Quote/helpers/functions.ts
@@ -1,7 +1,7 @@
-export function getRandom(min, max) {
+export function getRandom(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export function getRandomQuote(array) {
+export function getRandomQuote(array: any[]) {
   return array[getRandom(0, array.length)];
 }
