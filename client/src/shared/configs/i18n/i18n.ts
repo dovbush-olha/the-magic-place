5932c9f56f612ed345f794af5ef71b35c549d960
olha-dovbush <olha.dovbush@jito.dev>
2023-05-31 11:46:52 +0300

    [73] move public folder to client

--- client/src/shared/configs/i18n/i18n.ts
+++ client/src/shared/configs/i18n/i18n.ts
@@ -4,7 +4,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import home from '../../../../../public/locales/en/home.json';
+import home from '../../../../public/locales/en/home.json';
 
 export const defaultNS = 'home';
 export const resources = {
