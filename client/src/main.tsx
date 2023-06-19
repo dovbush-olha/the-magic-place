f9e4653e16b00f17e442d929c78edff0b0b0e6a9
olha-dovbush <olha.dovbush@jito.dev>
2023-06-19 15:48:36 +0300

    [75] install tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,22 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { GlobalStyle } from './app/style/globalStyles';
 import { routes } from './app/providers/router';
 import 'shared/configs/i18n/i18n';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
     <Suspense fallback={<div />}>
-      <RouterProvider router={routes} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routes} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Suspense>
   </React.StrictMode>
 );
